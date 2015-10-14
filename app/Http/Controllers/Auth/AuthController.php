09c8c7b76dbfbd07eaefc7bfbe33bb5c99d45ab2
Ademir Mazer Jr <ademir.mazer.jr@gmail.com>
2015-10-14 23:45:07 +0000

    Laravel 5.1.11 com HTML package

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -1,106 +1,65 @@
-<?php namespace App\Http\Controllers\Auth;
+<?php
 
-use Illuminate\Routing\Controller;
-use Illuminate\Contracts\Auth\Guard;
-use App\Http\Requests\Auth\LoginRequest;
-use App\Http\Requests\Auth\RegisterRequest;
+namespace App\Http\Controllers\Auth;
 
-/**
- * @Middleware("guest", except={"logout"})
- */
-class AuthController extends Controller {
+use App\User;
+use Validator;
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\ThrottlesLogins;
+use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
 
-	/**
-	 * The Guard implementation.
-	 *
-	 * @var Guard
-	 */
-	protected $auth;
+class AuthController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Registration & Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles the registration of new users, as well as the
+    | authentication of existing users. By default, this controller uses
+    | a simple trait to add these behaviors. Why don't you explore it?
+    |
+    */
 
-	/**
-	 * Create a new authentication controller instance.
-	 *
-	 * @param  Guard  $auth
-	 * @return void
-	 */
-	public function __construct(Guard $auth)
-	{
-		$this->auth = $auth;
-	}
+    use AuthenticatesAndRegistersUsers, ThrottlesLogins;
 
-	/**
-	 * Show the application registration form.
-	 *
-	 * @Get("auth/register")
-	 *
-	 * @return Response
-	 */
-	public function showRegistrationForm()
-	{
-		return view('auth.register');
-	}
+    /**
+     * Create a new authentication controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest', ['except' => 'getLogout']);
+    }
 
-	/**
-	 * Handle a registration request for the application.
-	 *
-	 * @Post("auth/register")
-	 *
-	 * @param  RegisterRequest  $request
-	 * @return Response
-	 */
-	public function register(RegisterRequest $request)
-	{
-		// Registration form is valid, create user...
+    /**
+     * Get a validator for an incoming registration request.
+     *
+     * @param  array  $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'name' => 'required|max:255',
+            'email' => 'required|email|max:255|unique:users',
+            'password' => 'required|confirmed|min:6',
+        ]);
+    }
 
-		$this->auth->login($user);
-
-		return redirect('/');
-	}
-
-	/**
-	 * Show the application login form.
-	 *
-	 * @Get("auth/login")
-	 *
-	 * @return Response
-	 */
-	public function showLoginForm()
-	{
-		return view('auth.login');
-	}
-
-	/**
-	 * Handle a login request to the application.
-	 *
-	 * @Post("auth/login")
-	 *
-	 * @param  LoginRequest  $request
-	 * @return Response
-	 */
-	public function login(LoginRequest $request)
-	{
-		if ($this->auth->attempt($request->only('email', 'password')))
-		{
-			return redirect('/');
-		}
-
-		return redirect('/auth/login')->withErrors([
-			'email' => 'These credentials do not match our records.',
-		]);
-	}
-
-	/**
-	 * Log the user out of the application.
-	 *
-	 * @Get("auth/logout")
-	 *
-	 * @return Response
-	 */
-	public function logout()
-	{
-		$this->auth->logout();
-
-		return redirect('/');
-	}
-
+    /**
+     * Create a new user instance after a valid registration.
+     *
+     * @param  array  $data
+     * @return User
+     */
+    protected function create(array $data)
+    {
+        return User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+        ]);
+    }
 }
--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -1,49 +1,44 @@
-<?php namespace App\Providers;
+<?php
+
+namespace App\Providers;
 
 use Illuminate\Routing\Router;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 
-class RouteServiceProvider extends ServiceProvider {
+class RouteServiceProvider extends ServiceProvider
+{
+    /**
+     * This namespace is applied to the controller routes in your routes file.
+     *
+     * In addition, it is set as the URL generator's root namespace.
+     *
+     * @var string
+     */
+    protected $namespace = 'App\Http\Controllers';
 
-	/**
-	 * The root namespace to assume when generating URLs to actions.
-	 *
-	 * @var string
-	 */
-	protected $rootUrlNamespace = 'App\Http\Controllers';
+    /**
+     * Define your route model bindings, pattern filters, etc.
+     *
+     * @param  \Illuminate\Routing\Router  $router
+     * @return void
+     */
+    public function boot(Router $router)
+    {
+        //
 
-	/**
-	 * The controllers to scan for route annotations.
-	 *
-	 * @var array
-	 */
-	protected $scan = [
-		'App\Http\Controllers\HomeController',
-		'App\Http\Controllers\Auth\AuthController',
-		'App\Http\Controllers\Auth\PasswordController',
-	];
+        parent::boot($router);
+    }
 
-	/**
-	 * Called before routes are registered.
-	 *
-	 * Register any model bindings or pattern based filters.
-	 *
-	 * @param  Router  $router
-	 * @return void
-	 */
-	public function before(Router $router)
-	{
-		//
-	}
-
-	/**
-	 * Define the routes for the application.
-	 *
-	 * @return void
-	 */
-	public function map(Router $router)
-	{
-		// require app_path('Http/routes.php');
-	}
-
+    /**
+     * Define the routes for the application.
+     *
+     * @param  \Illuminate\Routing\Router  $router
+     * @return void
+     */
+    public function map(Router $router)
+    {
+        $router->group(['namespace' => $this->namespace], function ($router) {
+            require app_path('Http/routes.php');
+        });
+    }
 }
